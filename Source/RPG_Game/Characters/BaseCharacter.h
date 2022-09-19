64e75caf2fedfd4b4ce98dc5fefd7f3428808f9b
Aleksandr Lapko <lapkoaleks@yandex.ru>
2022-09-19 23:08:41 +0300

    Setup player movement with als, add GAS plugin, create base classes

---
+++ Source/RPG_Game/Characters/BaseCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ALSCharacter.h"
+#include "BaseCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_GAME_API ABaseCharacter : public AALSCharacter
+{
+	GENERATED_BODY()
+	
+};
